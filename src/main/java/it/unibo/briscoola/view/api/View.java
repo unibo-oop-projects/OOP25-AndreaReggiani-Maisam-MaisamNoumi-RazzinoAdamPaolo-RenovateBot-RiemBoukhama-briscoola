2a6ae57c8967d35a2d8fe11ce6f528fd612d312e
Andrea Reggiani <andrea.reggiani14@studio.unibo.it>
2026-04-25 17:06:20 +0200

    briscoola/view/t: add View API and StartScreen implementation

---
+++ src/main/java/it/unibo/briscoola/view/api/View.java
@@ -0,0 +1,27 @@
+package it.unibo.briscoola.view.api;
+
+public interface View {
+    
+    /**
+     * Shows the inital screen
+     */
+    void start();
+
+    /**
+     * Initialize the game table
+     */
+    void initGame();
+
+    /**
+     * Updates the card count in a player's deck
+     * @param cardsCount
+     * @param player
+     */
+    void updatePile(int cardsCount, boolean player);
+
+    /**
+     * Shows amessage for the end of the game or the turn 
+     * @param message
+     */
+    void displayMessage(String message);
+}
---
+++ src/main/java/it/unibo/briscoola/view/impl/StartScreen.java
@@ -0,0 +1,47 @@
+package it.unibo.briscoola.view.impl;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class StartScreen extends JPanel{
+    public StartScreen(ActionListener onStart, ActionListener onQuit){
+        setLayout(new GridBagLayout());
+        setBackground(new Color(30,100,30));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10); /* this define empty space around the components / buttons */
+        gbc.gridx = 0; /*all in one central column */
+
+        /**
+         * to be sure.
+         * "gbc.gridy" defines the row (Y) of the grid in which to place the component.
+         * By increasing "gridy", components are stacked vertically. 
+         * "gridy" = 0 -> "title" , "gridy" = 1 -> "Button : Play" , "gridy" = 2 -> "Button : Exit"
+         */
+
+        
+        JLabel title = new JLabel("BRISCOOLA");
+        title.setFont(new Font("Serif", Font.BOLD, 60));
+        title.setForeground(Color.YELLOW);
+        gbc.gridy = 0;
+
+
+
+        JButton btnPlay = new JButton("Playy");
+        btnPlay.setPreferredSize(new Dimension(200, 50));
+        btnPlay.addActionListener(onStart);
+        gbc.gridy = 1;
+
+
+        JButton btnQuit = new JButton("Exit");
+        btnQuit.setPreferredSize(new Dimension(200, 50));
+        btnQuit.addActionListener(onQuit);
+        gbc.gridy = 2;
+
+        add(title, gbc);
+        add(btnPlay, gbc);
+        add(btnQuit, gbc);
+    }
+    
+}
